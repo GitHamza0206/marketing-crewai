5db31cbffdc99a0d5bf4e3e5c71be446f9e5e015
mikoba0206 <zeroualihamza0206@gmail.com>
2024-05-28 16:48:35 +0200

    commit 1

---
+++ main.py
@@ -0,0 +1,74 @@
+import os
+from crewai import Agent, Task, Crew, Process
+from langchain_community.tools import DuckDuckGoSearchRun
+from crewai_tools.tools import WebsiteSearchTool, SerperDevTool, FileReadTool
+import pandas as pd 
+import json 
+import streamlit as st
+seper_dev_tool = SerperDevTool()
+
+from dotenv import load_dotenv
+
+load_dotenv()
+def create_crew(OUTPUT_FOLDER):
+  # Load an Excel file into a DataFrame
+  #df = pd.read_excel('tasks.xlsx')
+  agent_load= json.loads(open("agents.json", "r").read())
+  agents={}
+  for agent in agent_load: 
+    agents[agent['agent_name']] = Agent(
+      role=agent['agent_name'],
+      backstory=agent['Backstory'],
+      goal=agent['Goal'],
+      allow_delegation=False,
+      verbose=True,
+    )
+  tasks={}
+  tasks_load=json.loads(open("tasks.json", "r").read())
+  for row in tasks_load:
+    tasks[row['task_variable_name']] = Task(
+      description=row['task_description'],
+      agent=agents[row['agent']],
+      memory=True,
+      final_answer='Your final answer must well formated and clear',
+      expected_output=row['excpected_output'],
+      output_file=f"{OUTPUT_FOLDER}/{row['doc_title']}.txt"
+    )
+  return agents, tasks 
+
+
+def crewai_setup( business_overview="I run a digital AI startup called YourMVP.com", start_with="ALL", OUTPUT_FOLDER="out"):
+  agents, tasks = create_crew(OUTPUT_FOLDER) 
+  
+  def create_tasks_list_from_start_with(start_with):
+    i=0
+    for task_name in tasks.keys():
+      if task_name == start_with:
+        return i 
+      i+=1 
+    return None 
+
+
+  if start_with=="ALL":
+  # Instantiate your crew with a sequential process
+    crew = Crew(
+      agents=[a for a in agents.values()],
+      tasks=[t for t in tasks.values()],
+      verbose=True
+    )
+  else: 
+    i = create_tasks_list_from_start_with(start_with)
+    crew = Crew(
+      agents=[a for a in agents.values()],
+      tasks=[t for t in tasks.values()][i:],
+      verbose=True
+    )
+
+  # Get your crew to work!
+  result = crew.kickoff({"business overview": business_overview })
+
+  print("######################")
+  return result
+
+if __name__=="__main__":
+  crewai_setup()
---
+++ webapp.py
@@ -0,0 +1,148 @@
+#display the console processing on streamlit UI
+import streamlit as st 
+import re
+from main import crewai_setup
+import sys 
+import os 
+import json 
+
+class StreamToExpander:
+    def __init__(self, expander):
+        self.expander = expander
+        self.buffer = []
+
+    def write(self, data):
+        # Filter out ANSI escape codes using a regular expression
+        cleaned_data = re.sub(r'\x1B\[[0-9;]*[mK]', '', data)
+
+        self.buffer.append(cleaned_data)
+        if "\n" in data:
+            self.expander.markdown(''.join(self.buffer))
+            self.buffer = []
+
+def load_tasks():
+    tasks={}
+    tasks_load=json.loads(open("tasks.json", "r").read())
+    return tasks_load
+
+# # Redirect stdout to our custom stream
+# sys.stdout = StreamToUI(st)
+# sys.stdout = sys.__stdout__
+
+def run_crewai_app():
+    
+    all_tasks = load_tasks()
+    with st.sidebar:
+        st.write('Steve Kaplan AI Logo Here ')
+        def refresh_page():
+            refresh_code = """
+                <script>
+                if (window.location !== window.parent.location) {
+                    window.parent.location.reload(true);
+                } else {
+                    window.location.reload(true);
+                }
+                </script>
+            """
+            st.markdown(refresh_code, unsafe_allow_html=True)
+
+# Create a button to trigger page refresh
+        if st.button("Refresh Page"):
+            refresh_page()
+
+
+
+    st.title("Mad Agence, Your AI Marketing department")
+    st.empty()
+    st.image('architecture.png', use_column_width="auto")
+    st.divider()
+    business_overview = st.text_area('Tell me about your business', placeholder="I run a digital marketing agency... ")
+
+    st.sidebar.header('⚙️ Parameters')
+
+    if "folder" not in st.session_state.keys():
+        st.session_state['folder'] = "Out10/"
+
+    if "prompts" not in st.session_state.keys():
+        st.session_state['prompts'] = "prompts.csv"
+
+    def on_click_btn(folder_name):
+        if not os.path.exists(folder_name):
+            os.makedirs(folder_name)
+        else:
+            st.error('Folder exists')
+    
+    def is_any_directory_not_empty(directories):
+        """Check if any of the specified directories is not empty."""
+        
+        if os.listdir(directories):
+            # Found a non-empty directory
+            return True
+        # All directories are empty
+        return False
+
+
+    # Allow users to select a folder
+    def file_selector(folder_path='.'):
+        folders = []
+        files = os.listdir(folder_path)
+        new_folder = '➕ new folder'
+        for folder in files:
+            flags =['.streamlit','__pycache__']
+            if os.path.isdir(folder) and folder not in flags:
+                folders.append(folder)
+        
+        folders.append(new_folder)
+        selected_filename = st.sidebar.selectbox('📁 Select a folder for saving', folders)
+        if selected_filename==folders[-1]: #if we select "add new folder"
+            fn,fb=st.sidebar.columns(2)
+            folder_name = fn.text_input('Folder name',placeholder="Folder name", label_visibility='collapsed')
+            add_btn = fb.button('Add')
+            if add_btn:
+                try:
+                    on_click_btn(folder_name=folder_name)
+                    st.experimental_rerun()
+                except:
+                    st.sidebar.error("Error with folder name ")
+                
+        elif is_any_directory_not_empty(selected_filename):
+            st.sidebar.warning("This folder is not empty! it's content will be erased ")
+        
+        return os.path.join(folder_path, selected_filename)
+    
+
+
+    filename = file_selector()
+    #filename+="/"
+    
+    st.sidebar.write('You selected `%s`' % filename)
+
+    all_tasks_name= ['ALL'] + [ i['task_variable_name'] for n,i in enumerate(all_tasks)]
+    
+    start_with = st.sidebar.selectbox('Begin with Task', options=all_tasks_name)
+
+    c1,c2=st.columns(2)    
+    run = c1.button("Run Analysis", type="primary")
+    if run:
+        stop=c2.button('stop', type="secondary")
+        if not stop:
+            with st.container(border=True):
+                sys.stdout = StreamToExpander(st)
+                
+                with st.spinner("Generating Results"):
+                    if business_overview:
+                        crew_result = crewai_setup(
+                            business_overview=business_overview,
+                            start_with = start_with,
+                            OUTPUT_FOLDER=filename
+                        )
+                    else:
+                        st.error("Business overview can't be empty")
+            
+        st.header("Results:")
+        st.markdown(crew_result)
+        with st.expander("Output"):
+            # Redirect stdout to our custom stream
+            sys.stdout = StreamToExpander(st)
+if __name__ == "__main__":
+    run_crewai_app()
